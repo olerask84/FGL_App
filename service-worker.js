695257b4df8d9a9e50c74ec04e3554e4fda0fa33
olerask84 <olerask84@gmail.com>
2026-04-07 13:39:28 +0200

    Update service worker cache version to v55

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// --- Service Worker for FGL (v51) ---
-const CACHE_NAME = 'fgl-cache-v51';
+// --- Service Worker for FGL (v55) ---
+const CACHE_NAME = 'fgl-cache-v55';
 const ASSETS = [
   './', './index.html', './styles.css', './app.js', './manifest.json',
   './assets/icons/FGL_192.png', './assets/icons/FGL_512.png', './assets/icons/FGL_192_VM.png'
