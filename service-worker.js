94ade26705a788fc2201ba6448ca993c904d4650
olerask84 <olerask84@gmail.com>
2026-08-05 10:46:16 +0200

    Update service worker cache version to v80

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// --- Service Worker for FGL (v79) ---
-const CACHE_NAME = 'fgl-cache-v79';
+// --- Service Worker for FGL (v80) ---
+const CACHE_NAME = 'fgl-cache-v80';
 const ASSETS = [
   './', './index.html', './styles.css', './app.js', './manifest.json',
   './assets/icons/FGL_192.png', './assets/icons/FGL_512.png', './assets/icons/FGL_192_VM.png'
